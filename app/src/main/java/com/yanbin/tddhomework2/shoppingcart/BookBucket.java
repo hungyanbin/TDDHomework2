ba0d185280ab42d2bca713dc208ce48e7c1fc673
hungyanbin <hung_yanbin@hotmail.com>
2015-10-27 23:48:33 +0800

    [what] Refacotring
    [how]  refactor BookBucket and make the test pass

--- app/src/main/java/com/yanbin/tddhomework2/shoppingcart/BookBucket.java
+++ app/src/main/java/com/yanbin/tddhomework2/shoppingcart/BookBucket.java
@@ -1,15 +1,29 @@
 package com.yanbin.tddhomework2.shoppingcart;
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by hungyanbin on 2015/10/27.
  */
 public class BookBucket {
 
+    private Map<Book, Integer> bucketMap = new HashMap<>();
+
     public void putBook(Book book){
+        if(bucketMap.containsKey(book)){
+            int bookCount = bucketMap.get(book);
+            bucketMap.put(book, bookCount + 1);
+        }else
+            bucketMap.put(book, 1);
     }
 
     public int getBookCount(Book book){
-        return 2;
+        for(Map.Entry<Book, Integer> bucket : bucketMap.entrySet()){
+            Book currentBook = bucket.getKey();
+            if(currentBook.equals(book))
+                return bucket.getValue();
+        }
+        return 0;
     }
 }
