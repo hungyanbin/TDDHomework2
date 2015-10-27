ff5377bb2dc75b90f16af083390073d4d883cb4c
hungyanbin <hung_yanbin@hotmail.com>
2015-10-27 23:43:51 +0800

    [what] 1.  Add new class BookBucket to manage books 2. Create BookBucketTest
    [how]  make the test pass

---
+++ app/src/Test/java/com/yanbin/tddhomework2/BookBucketTest.java
@@ -0,0 +1,34 @@
+package com.yanbin.tddhomework2;
+
+import com.yanbin.tddhomework2.shoppingcart.BookBucket;
+import com.yanbin.tddhomework2.shoppingcart.BookFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by hungyanbin on 2015/10/27.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(manifest = "app/src/main/AndroidManifest.xml", constants = BuildConfig.class, sdk = 21)
+public class BookBucketTest {
+
+    @Test
+    public void putBook_harryPotter_1_1_2_should_have_2_harryPotter1(){
+        //arrange
+        BookBucket target = new BookBucket();
+        int expectBookCount = 2;
+
+        //act
+        target.putBook(BookFactory.harryPotter(1));
+        target.putBook(BookFactory.harryPotter(1));
+        target.putBook(BookFactory.harryPotter(2));
+
+        //assert
+        Assert.assertEquals(expectBookCount, target.getBookCount(BookFactory.harryPotter(1)));
+    }
+}
+
---
+++ app/src/main/java/com/yanbin/tddhomework2/shoppingcart/BookBucket.java
@@ -0,0 +1,15 @@
+package com.yanbin.tddhomework2.shoppingcart;
+
+
+/**
+ * Created by hungyanbin on 2015/10/27.
+ */
+public class BookBucket {
+
+    public void putBook(Book book){
+    }
+
+    public int getBookCount(Book book){
+        return 2;
+    }
+}
